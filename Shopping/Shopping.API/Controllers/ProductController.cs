08ecd8f9509c9b6fef77363a2019509382243b64
Matteo Rigoni <78760991+MatteoRigoni@users.noreply.github.com>
2021-07-27 00:20:55 +0200

    Docker compose

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using Shopping.API.Data;
+using Shopping.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly ProductContext _context;
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger, ProductContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> GetAsync()
+        {
+            return await _context
+                .Products
+                .Find(p => true)
+                .ToListAsync();
+
+            //var rng = new Random();
+            //return Enumerable.Range(1, 5).Select(index => new Product()
+            //{
+            //    Name = $"Product {index}"
+            //}).ToArray();
+        }
+    }
+}
---
+++ Shopping/Shopping.API/Data/ProductContext.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using Shopping.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.API.Data
+{
+    public class ProductContext
+    {
+        public ProductContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration["DatabaseSettings:ConnectionString"]);
+            var database = client.GetDatabase(configuration["DatabaseSettings:DatabaseName"]);
+            Products = database.GetCollection<Product>(configuration["DatabaseSettings:CollectionName"]);
+            SeedData(Products);
+        }
+
+        private static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existsProduct = productCollection.Find(p => true).Any();
+            if (!existsProduct)
+            {
+                productCollection.InsertMany(GetPreconfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "Product 1",
+                    Description = "Description of product 1",
+                    Category = "Main category",
+                    ImageFile = "product1.png",
+                    Price = 100
+                },
+                new Product()
+                {
+                    Name = "Product 2",
+                    Description = "Description of product 2",
+                    Category = "Main category",
+                    ImageFile = "product2.png",
+                    Price = 200
+                },
+                new Product()
+                {
+                    Name = "Product 3",
+                    Description = "Description of product 3",
+                    Category = "Main category",
+                    ImageFile = "product3.png",
+                    Price = 8
+                }
+            };
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
